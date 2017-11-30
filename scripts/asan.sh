93f3b3f95cccf7a8c48f6f6f1ead8800319e7813
jvoisin <jvo@nbs-system.com>
2017-11-30 18:16:39 +0100

    Fix the path?

--- scripts/asan.sh
+++ scripts/asan.sh
@@ -2,9 +2,8 @@
 
 php_path="/usr/bin/php7.0"
 libasan_path="/usr/lib/x86_64-linux-gnu/libasan.so.3"
+libasan_path="/usr/lib/gcc/x86_64-linux-gnu/4.8/libasan.so"
 script_name="./preload_script"
-
-find / -name '*libasan*'
 
 _php_path=$(echo ${php_path} | sed 's/\//\\\//g')
 _script_name=$(echo ${script_name} | sed 's/\//\\\//g')
