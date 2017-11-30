e88a9ed27be9891f32a3c75756e82e0ad28b98e6
jvoisin <jvo@nbs-system.com>
2017-11-30 18:35:10 +0100

    Clean up a bit the script

--- scripts/asan.sh
+++ scripts/asan.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
 php_path="/usr/bin/php7.0"
-libasan_path="/usr/lib/x86_64-linux-gnu/libasan.so.3"
+#libasan_path="/usr/lib/x86_64-linux-gnu/libasan.so.3"
 libasan_path="/usr/lib/gcc/x86_64-linux-gnu/4.8/libasan.so"
 script_name="./preload_script"
 
 _php_path=$(echo ${php_path} | sed 's/\//\\\//g')
 _script_name=$(echo ${script_name} | sed 's/\//\\\//g')
 
-echo "#!/bin/sh
-LD_PRELOAD=\"${libasan_path}\" ${php_path} \"\$@\"" >  "../src/${script_name}"
-chmod +x "../src/${script_name}"
+[ -f ${php_path} ] || (echo "${php_path} doesn't exist"; exit)
+[ -f ${libasan_path} ] || (echo "${libasan_path} doesn't exist"; exit)
 
 cd ../src
+
+echo "#!/bin/sh
+LD_PRELOAD=\"${libasan_path}\" ${php_path} \"\$@\"" >  "${script_name}"
+chmod +x "${script_name}"
+
 phpize || exit
 CFLAGS="-fsanitize=address -fno-omit-frame-pointer -g -ggdb" CC="gcc" ./configure --enable-debug --enable-snuffleupagus  || exit
 sed -i -e "s/${_php_path}/${_script_name}/g" Makefile
