451d23a2c67694d3ac7ecb602c34da23a227f1f9
jvoisin <jvo@nbs-system.com>
2017-12-19 14:54:47 +0100

    Rework a bit the order of operation
    - There is no need to generate the key if the cookie has no value
    - There is no need to generate the key if the cookie length is invalid
    - Use yoda condition

--- src/sp_cookie_encryption.c
+++ src/sp_cookie_encryption.c
@@ -53,13 +53,13 @@ int decrypt_cookie(zval *pDest, int num_args, va_list args,
     return ZEND_HASH_APPLY_KEEP;
   }
 
-  generate_key(key);
-
-  if (Z_STRLEN_P(pDest) == 0) {
+	/* If the cookie has no value, it shouldn't be encrypted. */
+  if (0 == Z_STRLEN_P(pDest)) {
     return ZEND_HASH_APPLY_KEEP;
   }
 
-  debase64 = php_base64_decode((unsigned char *)(Z_STRVAL_P(pDest)), Z_STRLEN_P(pDest));
+  debase64 = php_base64_decode((unsigned char *)(Z_STRVAL_P(pDest)),
+			Z_STRLEN_P(pDest));
 
   if (ZSTR_LEN(debase64) <
       crypto_secretbox_NONCEBYTES + crypto_secretbox_ZEROBYTES) {
@@ -67,6 +67,8 @@ int decrypt_cookie(zval *pDest, int num_args, va_list args,
         "Buffer underflow tentative detected in cookie encryption handling.");
     return ZEND_HASH_APPLY_REMOVE;
   }
+
+  generate_key(key);
 
   decrypted = pecalloc(ZSTR_LEN(debase64), 1, 0);
 
