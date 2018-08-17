d538eef4fb62174ea32d94e28f1f5a20c5094426
xXx-caillou-xXx <sylvain.lefevre@epitech.eu>
2018-08-17 15:44:21 +0200

    Add ignore hash feature in `generate_rules.php` (#208)
    https://github.com/nbs-system/snuffleupagus/issues/206

--- scripts/generate_rules.php
+++ scripts/generate_rules.php
@@ -1,19 +1,29 @@
 <?php
 
-if ($argc != 2) {
-	echo 'Please provide a folder as argument.';
-	die();
+function help($name) {
+	die("Usage: $name [-h|--help] [--without-hash] folder\n");
+}
+
+if ($argc < 2) {
+	help($argv[0]);
 }
 
 $functions_blacklist = ['shell_exec', 'exec', 'passthru', 'php_uname', 'popen',
 	'posix_kill', 'posix_mkfifo', 'posix_setpgid', 'posix_setsid', 'posix_setuid',
 	'posix_setgid', 'posix_uname', 'proc_close', 'proc_nice', 'proc_open',
 	'proc_terminate', 'proc_open', 'proc_get_status', 'dl', 'pnctl_exec',
-	'pnctl_fork', 'assert', 'system', 'curl_exec', 'curl_multi_exec'];
+	'pnctl_fork', 'assert', 'system', 'curl_exec', 'curl_multi_exec', 'function_exists'];
 
 $extensions = ['php', 'php7', 'php5', 'inc'];
 
-$path = realpath($argv[1]);
+$path = realpath($argv[count($argv) - 1]);
+$parsedArgs = getopt('h', ['without-hash', 'help']);
+
+if (isset($parsedArgs['h']) || isset($parsedArgs['help'])) {
+	help($argv[0]);
+}
+
+$useHash = !isset($parsedArgs['without-hash']);
 
 $output = Array();
 
@@ -26,16 +36,17 @@ foreach($objects as $name => $object){
 	$hash = '';
 	$file_content = file_get_contents($name);
 
+	if ($useHash) {
+		$hash = '.hash("' . hash('sha256', $file_content) . '")';
+	}
+
 	foreach(token_get_all($file_content) as $token) {
 		if ($token[0] != 319) {
 			continue;
 		}
 
 		if (in_array($token[1], $functions_blacklist, true)) {
-			if ('' === $hash) {
-				$hash = hash('sha256', $file_content);
-			}
-			$output[] = 'sp.disable_function.function("' . $token[1] . '").filename("' . $name . '").hash("' . $hash . '").allow();' . "\n";
+			$output[] = 'sp.disable_function.function("' . $token[1] . '").filename("' . $name . '")' . $hash . '.allow();' . "\n";
 		}
 	}
 }
@@ -45,4 +56,4 @@ foreach($functions_blacklist as $fun) {
 
 foreach (array_unique($output) as $line) {
  	echo $line;
- }
+}
