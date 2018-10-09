a71e4205f31707e47d8eea950aba482049c999cb
jvoisin <julien.voisin@dustri.org>
2018-10-09 14:44:37 +0200

    Remove 3 useless lines

--- src/sp_upload_validation.c
+++ src/sp_upload_validation.c
@@ -83,9 +83,6 @@ int sp_rfc1867_callback(unsigned int event, void *event_data, void **extra) {
         sp_log_msg("upload_validation", sim ? SP_LOG_SIMULATION : SP_LOG_DROP,
                    "The upload of %s on %s was rejected.", filename,
                    uri ? uri : "?");
-        if (!config_upload->simulation) {
-          zend_bailout();
-        }
       }
     }
     ZEND_HASH_FOREACH_END();
