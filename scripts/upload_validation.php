19fcea2853bd95195b1c43e119ad8d870d82e587
Remi Collet <remi@famillecollet.com>
2018-12-15 15:35:21 +0100

    Provide a php script, to get rid of the python one for test suite
    This commit adds a php version of the upload_validation.py script.

---
+++ scripts/upload_validation.php
@@ -0,0 +1,45 @@
+#!/usr/bin/env php
+<?php
+
+function check($filename) {
+
+	$whitelist = ['ECHO', 'RETURN', 'PHP', 'NOP'];
+
+	$out = [];
+	$ret = 0;
+	$cmd = [
+		PHP_BINARY,
+		"-d", "vld.active=1",
+		"-d", "vld.execute=0",
+		"-d", "extension=vld.so",
+		"-d", "vld.format=1",
+		"-d", "vld.col_sep=@",
+		"-d", "log_errors=0",
+		"-d", "error_log=/dev/null",
+		escapeshellarg($filename),
+		'2>&1',
+		];
+	exec(implode(' ', $cmd), $out, $ret);
+	if ($ret) {
+        printf("Error: %d\n", $ret);
+		return 2;
+	}
+	foreach($out as $line) {
+		$sp = explode('@', $line);
+		if (count($sp) < 5) {
+			continue;
+		}
+		$opcode = $sp[4]; // # ,line, #, EIO, op, fetch, ext, return, operands
+		if ($opcode && !in_array($opcode, $whitelist)) {
+			printf("Upload_validation: Found an opcode: %s\n", $opcode);
+			return 1;
+		}
+	}
+	return 0;
+}
+
+if ($_SERVER['argc'] != 2) {
+	die("Usage: {$_SERVER['argv']['0']} file_to_test.php\n");
+}
+exit(check($_SERVER['argv']['1']));
+
