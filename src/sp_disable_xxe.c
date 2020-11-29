40abeff49e7db45bac3284bb54d4481c6cc6311e
jvoisin <julien.voisin@dustri.org>
2020-11-29 18:09:43 +0100

    Don't call libxml_disable_entity_loader for php8+
    This functions is deprecated, but since PHP8+ requires
    libxml 2.9.0 where XXE are disabled by default,
    there is no need to call it anymore.

--- src/sp_disable_xxe.c
+++ src/sp_disable_xxe.c
@@ -9,12 +9,15 @@ int hook_libxml_disable_entity_loader() {
 
   TSRMLS_FETCH();
 
+// External entities are disabled by default in PHP8+
+#if PHP_VERSION_ID < 80000
   /* Call the php function here instead of re-implementing it is a bit
    * ugly, but we do not want to introduce compile-time dependencies against
    * libxml. */
   ZVAL_STRING(&func_name, "libxml_disable_entity_loader");
   ZVAL_STRING(&params[0], "true");
   call_user_function(CG(function_table), NULL, &func_name, &hmac, 1, params);
+#endif
 
   HOOK_FUNCTION("libxml_disable_entity_loader", sp_internal_functions_hook,
                 PHP_FN(sp_libxml_disable_entity_loader));
