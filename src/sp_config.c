jvoisin/snuffleupagus

commit 7c2d1d7d2713c0fa6bda63c376baf25d9f3d712c

More const frenzy

src/sp_config.c
@@ -6,7 +6,7 @@
 
 
 static zend_result sp_process_config_root(sp_parsed_keyword *parsed_rule) {
-  sp_config_keyword sp_func[] = {
+  static const sp_config_keyword sp_func[] = {
     {parse_unserialize,         SP_TOKEN_UNSERIALIZE_HMAC, &(SPCFG(unserialize))},
     {parse_enable,              SP_TOKEN_HARDEN_RANDOM, &(SPCFG(random).enable)},
     {parse_log_media,           SP_TOKEN_LOG_MEDIA, &(SPCFG(log_media))},
@@ -29,7 +29,7 @@ static zend_result sp_process_config_root(sp_parsed_keyword *parsed_rule) {
   return sp_process_rule(parsed_rule, sp_func);
 }
 
-zend_result sp_parse_config(const char *filename) {
+zend_result sp_parse_config(const char *const filename) {
   FILE *fd = fopen(filename, "rb");
   if (fd == NULL) {
     sp_log_err("config", "Could not open configuration file %s : %s", filename, strerror(errno));
@@ -65,7 +65,7 @@ zend_result sp_parse_config(const char *filename) {
 }
 
 
-zend_result sp_process_rule(sp_parsed_keyword *parsed_rule, const sp_config_keyword *config_keywords) {
+zend_result sp_process_rule(sp_parsed_keyword *parsed_rule, const sp_config_keyword *const config_keywords) {
   for (sp_parsed_keyword *kw = parsed_rule; kw->kw; kw++) {
     bool found_kw = false;
     for (const sp_config_keyword *ckw = config_keywords; ckw->func; ckw++) {
@@ -119,13 +119,12 @@ SP_PARSEKW_FN(parse_list) {
   CHECK_DUPLICATE_KEYWORD(retval);
 
   sp_list_node **list = retval;
-  char *tok, *tmp;
 
   SP_PARSE_ARG(value);
 
-  tmp = ZSTR_VAL(value);
+  char* tmp = ZSTR_VAL(value);
   while (1) {
-    tok = strsep(&tmp, ",");
+    const char* const tok = strsep(&tmp, ",");
     if (tok == NULL) {
       break;
     }

src/sp_config.h
@@ -176,7 +176,7 @@ typedef struct {
   HashTable *entries;  // ht of sp_ini_entry
 } sp_config_ini;
 
-#define SP_PARSE_FN_(fname, kwvar) int fname(char *token, sp_parsed_keyword *kwvar, void *retval)
+#define SP_PARSE_FN_(fname, kwvar) int fname(char const *const token, sp_parsed_keyword *kwvar, void *retval)
 #define SP_PARSE_FN(fname) SP_PARSE_FN_(fname, parsed_rule)
 #define SP_PARSEKW_FN(fname) SP_PARSE_FN_(fname, kw)
 
@@ -269,9 +269,9 @@ typedef struct {
 
 #define SP_TOKEN_LIST "list"
 
-zend_result sp_process_rule(sp_parsed_keyword *parsed_rule, const sp_config_keyword *config_keywords);
+zend_result sp_process_rule(sp_parsed_keyword *parsed_rule, const sp_config_keyword *const config_keywords);
 
-zend_result sp_parse_config(const char *filename);
+zend_result sp_parse_config(const char *const filename);
 
 #define SP_PARSE_CHECK_ARG_EXISTS(value) \
 if (!value) { \

commit e73c4c7618f0e5db2e0637a7f43846ea53fbf784

Fix ZTS build
Bug introduced in https://github.com/jvoisin/snuffleupagus/commit/7c2d1d7d2713c0fa6bda63c376baf25d9f3d712c

src/sp_config.c
@@ -6,7 +6,7 @@
 
 
 static zend_result sp_process_config_root(sp_parsed_keyword *parsed_rule) {
-  static const sp_config_keyword sp_func[] = {
+  sp_config_keyword sp_func[] = {
     {parse_unserialize,         SP_TOKEN_UNSERIALIZE_HMAC, &(SPCFG(unserialize))},
     {parse_enable,              SP_TOKEN_HARDEN_RANDOM, &(SPCFG(random).enable)},
     {parse_log_media,           SP_TOKEN_LOG_MEDIA, &(SPCFG(log_media))},